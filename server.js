8b7165c563079d6b0def930d2e09c5c0b4fae260
bbeck9123 <byron.ptgo@gmail.com>
2023-07-28 10:35:09 -0700

    news

---
+++ server.js
@@ -0,0 +1,16 @@
+letsDothis function(int, parts){
+    let divisor = Math.floor(int/parts)
+    let remainder = int % parts
+    let arr = []
+
+    for(let i=0; i < parts-remainder; i++){
+        arr.push[divisor]
+    }
+
+    for(let i=0; i < remainder; i++){
+        arr.push[divisor+1]
+    }
+
+
+
+}
